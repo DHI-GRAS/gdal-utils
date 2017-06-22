6ca002a71213f25b50990a082b89414b02553eb0
Jonas Solvsteen <josl@dhigroup.com>
2017-06-22 16:07:57 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gdal_utils',
-    version='0.2',
+    version='0.3',
     description='GDAL and OGR Utilities',
     author='Jonas Solvsteen',
     author_email='josl@dhi-gras.com',
